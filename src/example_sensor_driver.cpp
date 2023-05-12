ade81c403b2f7685c29f8a9a32c64c8efa87ac04
Alex-Carney <alexander.s.carney.ug@dartmouth.edu>
2023-05-12 16:02:44 -0400

    Added characterization for third sensor

--- src/example_sensor_driver.cpp
+++ src/example_sensor_driver.cpp
@@ -3,16 +3,21 @@
 
 const int sensor1Pin = A11;
 const int sensor2Pin = A10;
+const int sensor3Pin = A9;
 
 // Characterized in MATLAB
-// SENSOR 1 is the sensor with serial number 2003
+// SENSOR 1 is the sensor with with a single tick on the right
 float sensor1_coefficients[] = {2.2e4, -1.227};
-// SENSOR 2 is the sensor with serial number 3006
+// SENSOR 2 is the sensor with two tick marks on it (long)
 float sensor2_coefficients[] = {1.18e4, -1.138};
+// SENSOR 3 is the sensor with the big circle on it
+float sensor3_coefficients[] = {1.3e4, -1.149};
+
 
 // Instantiate position sensors
-PositionSensor sensor1(sensor1Pin, sensor1_coefficients);
-PositionSensor sensor2(sensor2Pin, sensor2_coefficients);
+PositionSensor sensor_mini_right(sensor1Pin, sensor1_coefficients);
+PositionSensor sensor_marks(sensor2Pin, sensor2_coefficients);
+PositionSensor sensor_big_circle(sensor3Pin, sensor3_coefficients);
 
 void setup()
 {
@@ -21,8 +26,8 @@ void setup()
 
 void loop()
 {
-    float distanceRAW = sensor2.readDistanceRaw();
-    float distanceCM = sensor2.readDistanceCM();
+    float distanceRAW = sensor_big_circle.readDistanceRaw();
+    float distanceCM = sensor_big_circle.readDistanceCM();
 
     Serial.print("Distance in cm: ");
     Serial.print(distanceCM);
