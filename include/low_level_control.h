4b76a20a1e52ca2c36a45feae7b5ad8637934066
Alex-Carney <alexander.s.carney.ug@dartmouth.edu>
2023-05-17 16:59:58 -0400

    Scaffolded pos control integration

--- include/low_level_control.h
+++ include/low_level_control.h
@@ -10,6 +10,7 @@
 #include <Arduino.h>
 #include "ArduinoMotorShieldR3.h"
 #include "CircularBuffer.h"
+#include "MotorCommand.h"
 
 namespace ll_control
 {
@@ -18,7 +19,7 @@ namespace ll_control
      *
      * @author Youssef Marzouk
      */
-    void updateWheelSpeed(
+    MotorCommand compensateDriveSpeed(
         double ref_right,
         double ref_left,
         long motor1_pos,
@@ -28,8 +29,22 @@ namespace ll_control
         long encoder3Val_start_m1,
         long encoder3Val_start_m2,
         CircularBuffer<double>& control_buffer_m1,
-        CircularBuffer<double>& control_buffer_m2,
-        ArduinoMotorShieldR3& md);
+        CircularBuffer<double>& control_buffer_m2);
+    /**
+     * @brief Sets the car's position to the given value
+     * 
+     * @author Nick Hagler
+    */
+    MotorCommand compensateMousePosition(
+      double pos_ref,
+      long motor1_pos,
+      long motor2_pos,
+      long newTime,
+      long oldTime,
+      long encoder3Val_start_m1,
+      long encoder3Val_start_m2,
+      CircularBuffer<double>& control_buffer_m1,
+      CircularBuffer<double>& control_buffer_m2);
 }
 
 #endif // LOW_LEVEL_CONTROL_H
