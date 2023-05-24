d04d93fce2f719ea9d59b2785921b00301ee5fea
Alex <alexander.s.carney.ug@dartmouth.edu>
2023-05-24 00:22:24 -0400

    Updated low level h

--- include/low_level_control.h
+++ include/low_level_control.h
@@ -28,24 +28,51 @@ namespace ll_control
         long oldTime,
         long encoder3Val_start_m1,
         long encoder3Val_start_m2,
-        CircularBuffer<double>& control_buffer_m1,
-        CircularBuffer<double>& control_buffer_m2);
+        CircularBuffer<double> &control_buffer_m1,
+        CircularBuffer<double> &control_buffer_m2);
     /**
      * @brief Sets the car's position to the given value
-     * 
+     *
      * @author Nick Hagler
-    */
+     */
     MotorCommand compensateMousePosition(
-      double pos_left,
-      double pos_right,
-      long motor1_pos,
-      long motor2_pos,
-      long newTime,
-      long oldTime,
-      long encoder3Val_start_m1,
-      long encoder3Val_start_m2,
-      CircularBuffer<double>& control_buffer_m1,
-      CircularBuffer<double>& control_buffer_m2);
+        double pos_left,
+        double pos_right,
+        long motor1_pos,
+        long motor2_pos,
+        long newTime,
+        long oldTime,
+        long encoder3Val_start_m1,
+        long encoder3Val_start_m2,
+        CircularBuffer<double> &control_buffer_m1,
+        CircularBuffer<double> &control_buffer_m2);
+
+    /**
+     * @brief Sets the car's rotational position to the given value
+     *
+     * @author Youssef Marzouk
+     */
+    MotorCommand compensateTurning(
+        long motor1_pos,
+        long motor2_pos,
+        long newTime,
+        long oldTime,
+        long encoder3Val_start_m1,
+        long encoder3Val_start_m2,
+        double turn_start,
+        double heading_meas,
+        BLA::Matrix<2, 2> A,
+        BLA::Matrix<2, 2> B,
+        BLA::Matrix<2, 2> C,
+        BLA::Matrix<2, 4> F_bar,
+        BLA::Matrix<2, 1> y_star,
+        BLA::Matrix<2, 1> yk,
+        BLA::Matrix<2, 1> xk,
+        BLA::Matrix<2, 1> xkp,
+        BLA::Matrix<2, 1> zk,
+        BLA::Matrix<2, 1> zkp,
+        BLA::Matrix<4, 1> wk,
+        BLA::Matrix<2, 1> u);
 }
 
 #endif // LOW_LEVEL_CONTROL_H
